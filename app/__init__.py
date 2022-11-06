91794ac7d8a4870c8d5661168b29ddfb9906b055
Obcido <jonathan.harkonen@gmail.com>
2022-11-06 19:45:53 +0200

    basic base stuff

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__, static_folder=None)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+
+
+CORS(app)
+
+
+from app.orders import orders
+app.register_blueprint(orders, url_prefix='/orders')
+
+from app.services import services
+app.register_blueprint(services, url_prefix='/services')
+
+from app.cabins import cabins
+app.register_blueprint(cabins, url_prefix='/cabins')
